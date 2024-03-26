dc011df28658629d9370d8584a294a6b376aa75d
Mukremin GOKDUMAN <mukremingokduman@gmail.com>
2024-03-27 00:38:02 +0300

    edited commend line in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-//import '../styles/themes.dart';
 import '../contants/routes.dart';
 import '../contants/globals.dart';
 import '../types/localication_type.dart';
